4dcc43715bba50cffbd75cd0858771156887a3bf
Niharika-1505 <gaddeniharika88@gmail.com>
2021-11-02 13:53:06 +0000

    BMI 2.0, Leap year, Love calculator, Pizza order, Treasure island

--- Day3/BMI2.0.py
+++ Day3/BMI2.0.py
@@ -3,16 +3,16 @@ height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 
 BMI = weight / (height ** 2)
-if BMI <18.5 :
-  print(f"Your BMI is {BMI}, you are underweight")
-elif BMI > 18.5 and BMI < 25 :
-  print(f"Your BMI is {BMI}, you have a normal weight")
-elif BMI > 25 and BMI < 30 :
-  print(f"Your BMI is {BMI}, you are slightly overweight.")
-elif BMI > 30 and BMI < 35 :
-  print(f"Your BMI is {BMI}, you are obese.")
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight")
+elif 18.5 < BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight")
+elif 25 < BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif 30 < BMI < 35:
+    print(f"Your BMI is {BMI}, you are obese.")
 else:
-  print(f"Your BMI is {BMI}, you are clinically obese.")
+    print(f"Your BMI is {BMI}, you are clinically obese.")
