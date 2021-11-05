8ac6a64d8c4e7622d2419eb9be2848d30fa80786
Niharika-1505 <gaddeniharika88@gmail.com>
2021-11-05 13:25:59 +0000

    Coffee machine project using nested dictionaries

--- Day15/CoffeeMachineProject.py
+++ Day15/CoffeeMachineProject.py
@@ -36,7 +36,7 @@ def is_transaction_successful(money_received, drink_cost):
 
 
 def make_coffee(drink_name, order_ingredients):
-    """Deduct the required ingredients from the resources."""
+    """Deduct the required ingredients from the resources..."""
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
     print(f"Here is your {drink_name} ☕️. Enjoy!")
