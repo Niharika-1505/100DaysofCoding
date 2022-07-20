19442694b1299befff4e70cd9e05bbd3892d7fdc
Niharika-1505 <gaddeniharika88@gmail.com>
2022-07-20 12:19:28 +0100

    NHS_&_PHS_Digital_WebDataExtraction, webscrapping, data cleaning, extracting a file from zip folder,

---
+++ ExtraPractice/NHS_&_PHS_Digital_WebDataExtraction/NursePrescribersInEngland.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+from urllib.request import urlopen
+from io import BytesIO
+
+'''https://blog.finxter.com/a-step-by-step-guide-to-fetching-the-url-from-the-href-attribute-using-beautifulsoup/
+https://stackoverflow.com/questions/5710867/downloading-and-unzipping-a-zip-file-without-writing-to-disk'''
+
+response = requests.get(url="https://digital.nhs.uk/services/organisation-data-service/file-downloads/gp-and-gp"
+                            "-practice-related-data")
+html_contents = response.text
+soup = BeautifulSoup(html_contents, "html.parser")
+
+# Getting href link to download zip file
+all_articles = soup.find(href=True, string="enurse")
+Url_to_download_zip = all_articles['href']
+
+# Write to CSV file after reading csv file from the zip folder
+response = urlopen(Url_to_download_zip)
+zipfile = ZipFile(BytesIO(response.read()))
+print(zipfile.namelist())
+zipfile.extract("enurse.csv")
+
---
+++ ExtraPractice/NHS_&_PHS_Digital_WebDataExtraction/PHS_National_Reference_Files.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+'''https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python'''
+
+response = requests.get(url="https://www.isdscotland.org/Products-and-Services/Data-Definitions-and-References"
+                            "/National-Reference-Files/")
+html_contents = response.text
+soup = BeautifulSoup(html_contents, "html.parser")
+
+# Getting href link
+all_articles = soup.find(href=True, string="purchpro")
+Url_to_Extract_purchpro = all_articles['href']
+
+# To read data from href into a List
+purchpro_raw_webdata = urlopen(Url_to_Extract_purchpro)
+purchpro_webdata_List = purchpro_raw_webdata.readlines()
+
+# Removing leading and trailing characters from each element in the list
+list_after_removing_starting_2_chars = list(map(lambda x: str(x)[2:], purchpro_webdata_List))
+list_after_removing_ending_10_chars = list(map(lambda x: str(x)[:-10], list_after_removing_starting_2_chars))
+
+# Removing extra whitespaces from each element in the list and creating a final list
+final_purchproList = []
+for each_element in list_after_removing_ending_10_chars:
+    data = " ".join(each_element.split())
+    final_purchproList.append(data)
+
+# To write the List to DataFrame
+df = pd.DataFrame(final_purchproList)
+
+# To write the DataFrame to CSV
+df.to_csv("purchpro.csv", index=False)
